2dcee5167a3cca446887a0d1808cf0529424bd93
jleow <jleow@iupui.edu>
2017-06-08 15:38:52 -0400

    Build list item by cloning a template element

--- app.js
+++ app.js
@@ -31,10 +31,13 @@ const app = {
   },
 
   renderListItem(dino) {
-    const item = document.createElement('li')
-    item.textContent = dino.name
-
+    const item = this.template.cloneNode(true)
+    item.classList.remove('template')
     item.dataset.id = dino.id
+    item
+      .querySelector('.dino-name')
+      .textContent = dino.name
+
     return item
   }
 }
