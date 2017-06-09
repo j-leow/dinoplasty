8480b441d09cb5361d02d86a8b854b5cd5abb1c4
Jules <jleow@iupui.edu>
2017-06-08 21:59:08 -0400

    Fixed dino id value after removeDino()

--- app.js
+++ app.js
@@ -2,16 +2,47 @@ const app = {
   init(selectors) {
     this.dinos = []
     this.max = 0
-    this.list = document.querySelector(selectors.listSelector)
-    document
-      .querySelector(selectors.formSelector)
-      .addEventListener('submit', this.addDino.bind(this))
+    this.list = document
+      .querySelector(selectors.listSelector)
     this.template = document
       .querySelector(selectors.templateSelector)
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener('submit', this.addDinoFromForm.bind(this))
+
+    this.load()
 
   },
 
-  addDino(ev) {
+  load() {
+    // load the JSON from localStorage
+    const dinoJSON = localStorage.getItem('dinos')
+
+    // convert the JSON back into an array
+    const dinoArray = JSON.parse(dinoJSON)
+
+    // set this.dinos with the dinos from that array
+    if (dinoArray) {
+      dinoArray
+        .reverse()
+        .map(this.addDino.bind(this))
+    }
+  },
+
+  addDino(dino) {
+    const listItem = this.renderListItem(dino)
+    this.list.insertBefore(listItem, this.list.firstChild)
+
+    this.dinos.unshift(dino)
+
+    if (dino.id > this.max) {
+      this.max = dino.id
+    }
+console.log(dino.id)
+    this.save()
+  },
+
+  addDinoFromForm(ev) {
     ev.preventDefault()
 
     const dino = {
@@ -19,14 +50,8 @@ const app = {
       name: ev.target.dinoName.value,
     }
 
-    const listItem = this.renderListItem(dino)
-    this.list.insertBefore(listItem, this.list.firstChild)
-    this.list.appendChild(listItem)
-
-    this.dinos.unshift(dino)
-    this.save()   //this - the object because we called it by method
-
-    ++ this.max
+    console.log(dino.id)
+    this.addDino(dino)
 
     ev.target.reset()
   },
@@ -34,15 +59,17 @@ const app = {
   save() {
     localStorage
       .setItem('dinos', JSON.stringify(this.dinos))
-  }
+  },
 
   renderListItem(dino) {
     const item = this.template.cloneNode(true)
     item.classList.remove('template')
     item.dataset.id = dino.id
+
     item
       .querySelector('.dino-name')
       .textContent = dino.name
+
     item
       .querySelector('button.remove')
       .addEventListener('click', this.removeDino.bind(this))
@@ -54,14 +81,15 @@ const app = {
     const listItem = ev.target.closest('.dino')
     listItem.remove()
 
-    for (let i=0; i < this.dinos.length; i++)
-    {
+
+    for (let i = 0; i < this.dinos.length; i++) {
       const currentId = this.dinos[i].id.toString()
       if (listItem.dataset.id === currentId) {
         this.dinos.splice(i, 1)
         break;
       }
     }
+
     this.save()
   },
 }
@@ -69,5 +97,5 @@ const app = {
 app.init({
   formSelector: '#dino-form',
   listSelector: '#dino-list',
-  templateSelector: '.dino.template'
+  templateSelector: '.dino.template',
 })
